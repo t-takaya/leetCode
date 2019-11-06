cd6fb9077f4bc2a3acb338eb0a14df11c71ea609
“Takuya <tukamo_n700@yahoo.co.jp>
2019-11-07 00:03:00 +0900

    add TransposeMatrixを追加

---
+++ src/main/scala/easy/TransposeMatrix.scala
@@ -0,0 +1,9 @@
+package easy
+
+// Result
+// https://leetcode.com/submissions/detail/276513134/
+object TransposeMatrix {
+  def transpose(A: Array[Array[Int]]): Array[Array[Int]] = {
+    A.transpose
+  }
+}
